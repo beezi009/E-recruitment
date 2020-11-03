5769a560ab308be513002dc824ae3ea0eb71fb0d
Beezi Mukupi <beezimukupi@gmail.com>
2020-11-03 23:04:02 +0200

    Add files via upload
    Dashboard Page

---
+++ welcome.php
@@ -0,0 +1,86 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="index.php">
+    <img src="images/1200px-Coat_of_arms_of_Namibia.svg.png" alt="logo" style="width:40px;">
+  </a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Vacancies</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="details.php">Personal Details</a>
+    </li>
+    <li class="nav-item">
+    <li class="nav-item">
+      <a class="nav-link" href="education.php">Educational Details</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="work.php">Work History</a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="#">Job Applications</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="uploads.php">Documents</a>
+    </li>
+
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
+  </ul>
+</nav>
+
+<div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    </div>
+<div class="row">
+  <div class="col-sm-6 bg-warning">
+        <img src="images/pexels-cottonbro-5081971.jpg" width="100%">
+      </div>
+      <div class="col-sm-6 bg-warning">
+        <img src="images/pexels-cottonbro-5081971.jpg" width="100%">
+      </div>
+  
+</div>
+    
+
+
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>Research Project 2020</p>
+</div>
+
+
+</body>
+</html>
