652c8482d772d635779c9497d6a2c45e31cc5d22
Beezi Mukupi <beezimukupi@gmail.com>
2020-11-04 21:41:35 +0200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -11,7 +11,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Dashboard</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
