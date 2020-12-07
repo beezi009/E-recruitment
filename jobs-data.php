973fde4b4d7602d7f0daec011e7fffc0f6c83e8a
Beezi Mukupi <beezimukupi@gmail.com>
2020-12-08 00:28:30 +0200

    Update jobs-data.php

--- jobs-data.php
+++ jobs-data.php
@@ -7,16 +7,23 @@ $username = $_SESSION["username"];
 
    $title = $_POST['title'];
    $description = $_POST['description'];
-   $date_posted = $_POST['date_posted'];
    $closing_date = $_POST['closing_date'];
+   $ministry = $_POST['ministry'];
+   $location = $_POST['location'];
 
-   $sql = "INSERT INTO jobs (title, description, date_posted, closing_date )
-   VALUES ('$title','$description','$date_posted', '$closing_date')";
+   $sql = "INSERT INTO jobs (title, description, date_posted, closing_date, username, location, ministry )
+   VALUES ('$title','$description', NOW(), '$closing_date', '$username', '$location', '$ministry' )";
    if (mysqli_query($link, $sql)) {
-    echo "New record created successfully !";
+    echo "<script>
+alert('New record created successfully !') 
+window.location.href='jobs.php';
+</script>";
    } else {
     echo "Error: " . $sql . "
 " . mysqli_error($link);
    }
    mysqli_close($link);
+
+
+
 ?>
