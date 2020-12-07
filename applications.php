92510574b479b2d125c1e2bb7292dea1c15227cf
Beezi Mukupi <beezimukupi@gmail.com>
2020-12-07 20:50:33 +0200

    Update applications.php

--- applications.php
+++ applications.php
@@ -23,23 +23,12 @@
   <!-- Links -->
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" href="index.php">Vacancies</a>
+      <a class="nav-link" href="jobs.php">Post vacancies</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="Details.php">Personal Details</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="education.php">Educational Details</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="work.php">Work History</a>
-    </li>
+   
 
     <li class="nav-item">
-      <a class="nav-link" href="jobs.php">Job Applications</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="uploads.php">Documents</a>
+      <a class="nav-link" href="applications.php">Job Applications</a>
     </li>
 
     <p>
@@ -53,31 +42,45 @@
     <div class="row">
       <div class="col-sm-6">
         <h3>Received Job Applications:</h3>
+        <form action="/action_page.php">
+    <select name="cars" class="custom-select mb-3">
+      <option selected>Select Ministry</option>
+      <option value="volvo">Volvo</option>
+      <option value="fiat">Fiat</option>
+      <option value="audi">Audi</option>
+      </select>
+
+      <select name="cars" class="custom-select mb-3">
+      <option selected>Select Job</option>
+      <option value="volvo">Volvo</option>
+      <option value="fiat">Fiat</option>
+      <option value="audi">Audi</option>
+    </select>
+    
+    <button type="submit" class="btn btn-primary">Search</button>
+  </form>
         <p>Here is a list of short listed candidates.</p>
     <table class="table table-bordered">
     <thead>
       <tr>
-        <th>#</th>
-        <th>Position</th>
-        <th>Date Posted</th>
-        <th>Status</th>
-        <th>Delete</th>
+        <th>First Name</th>
+        <th>Last name</th>
+        <th>Score</th>
+        <th>View</th>
       </tr>
     </thead>
     <?php
                     include 'config.php';
-                    $sql = "SELECT * from work_history ";
+                    $sql = "SELECT first_names, last_name, id_number, candidate_score from user_data, candidates ";
                     $result = mysqli_query($link,$sql);
                     while($row = mysqli_fetch_assoc($result)){
                              echo '
     <tbody>
       <tr>
-        <td>'.$row['company_name'].'</td>
-        <td>'.$row['position'].'</td>
-        <td>'.$row['start_year'].'</td>
-        <td>'.$row['end_year'].'</td>
-        <td>'.$row['employement_type'].'</td>
-        <td><a href="process_delete_work.php?id='.$row['id'].'"><span class="glyphicon glyphicon-trash"></span></td>
+        <td>'.$row['first_names'].'</td>
+        <td>'.$row['last_name'].'</td>
+        <td>'.$row['candidate_score'].'</td>
+        <td><a href="candidate-single.php?id_number='.$row['id_number'].'">Open</td>
 
       </tr>
 
